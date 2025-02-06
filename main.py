214c15ab690884df94f7ea939d76da6583b38136
Batman <anuragsing092@gmail.com>
2025-02-06 22:54:42 +0545

    get_number_of_line

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+MAX_LINES = 3
 def deposit():
     while True:
         amount = input("What would you like to deposit? $")
@@ -9,7 +9,27 @@ def deposit():
             else:
                 print("Amount must be greater than 0.")
         else:
-            print("Plese enter a number.")
+            print("Please enter a number.")
 
     return amount
 
+def get_number_of_line():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" +str(MAX_LINES) +)
+        if lines.isdigit():
+            lines = int(lines)
+            if 1<= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+
+    return lines
+
+def  main():
+    balance = deposit()
+    lines = get_number_of_line()
+
+
+main()
